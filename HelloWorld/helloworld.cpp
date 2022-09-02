771fc922c0eb4823ac28a76e5157c0ce77347a6a
Tyler-Seliber <tylerseliber@gmail.com>
2022-09-02 14:51:02 -0400

    Lecture 9/2

---
+++ HelloWorld/helloworld.cpp
@@ -0,0 +1,19 @@
+// iostream = input/output stream - contains definitions for std and therefore cout
+#include <iostream>
+// using namespace std - allows us to use cout without std::cout
+using namespace std;
+
+// main function **must** return integer. 0 is the convention for success.
+int main()
+{
+    // Print
+    // cout is an object - output stream. it represents the screen of your computer
+    // << is the insertion operator for sending data to an object
+    cout << "hello world" << endl;
+    cout << 345 << "oiewrgowerg" << endl;
+    cout << "enter a string: ";
+    string s;
+    cin >> s;
+    cout << "s is: " << s << endl;
+    return 0;
+}
