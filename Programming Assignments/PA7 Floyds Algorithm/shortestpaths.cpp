3099660c03b84e4d79a1e34532967b0990d49657
Tyler-Seliber <tylerseliber@gmail.com>
2022-12-04 19:50:58 -0500

    PA7 Setup

---
+++ Programming Assignments/PA7 Floyds Algorithm/shortestpaths.cpp
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Filename: shortestpaths.cpp
+ * Author  : Hyeonu Ju & Tyler Seliber
+ * Version : 1.0
+ * Date    : December 7, 2022
+ * Description: Solves the all-pairs shortest paths problem with Floyd's algorithm.
+ * Pledge  : I pledge my honor that I have abided by the Stevens Honor System.
+ ******************************************************************************/
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+    // Make sure the right number of command line arguments exist.
+    if (argc != 2)
+    {
+        cerr << "Usage: " << argv[0] << " <filename>" << endl;
+        return 1;
+    }
+    // Create an ifstream object.
+    ifstream input_file(argv[1]);
+    // If it does not exist, print an error message.
+    if (!input_file)
+    {
+        cerr << "Error: Cannot open file '" << argv[1] << "'." << endl;
+        return 1;
+    }
+    // Add read errors to the list of exceptions the ifstream will handle.
+    input_file.exceptions(ifstream::badbit);
+    string line;
+    try
+    {
+        unsigned int line_number = 1;
+        // Use getline to read in a line.
+        // See http://www.cplusplus.com/reference/string/string/getline/
+        while (getline(input_file, line))
+        {
+            cout << line_number << ":\t" << line << endl;
+            ++line_number;
+        }
+        // Don't forget to close the file.
+        input_file.close();
+    }
+    catch (const ifstream::failure &f)
+    {
+        cerr << "Error: An I/O error occurred reading '" << argv[1] << "'.";
+        return 1;
+    }
+
+    return 0;
+}
